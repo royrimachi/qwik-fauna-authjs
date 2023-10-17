f2b8a2a95c5507854b1d0ac82d6a937e5108a2c3
Roy Joseph Rimachi Costillas <royexrc@gmail.com>
2023-10-17 11:54:10 -0500

    feat: auth js and sign in|out added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,51 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { type DocumentHead } from "@builder.io/qwik-city";
+import { useAuthSession, useAuthSignin, useAuthSignout } from "./plugin@auth";
 
 export default component$(() => {
+  const session = useAuthSession();
+  const signIn = useAuthSignin();
+  const signOut = useAuthSignout();
+
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      {session.value && session.value.user ? (
+        <>
+          <h1>Hi 👋</h1>
+          <p>
+            Can't wait to see what you build with qwik!
+            <br />
+            Happy coding.
+          </p>
+          <p>Session expires at {session.value?.expires?.toLocaleString()}</p>
+          {session.value.user.image && session.value.user.name ? (
+            <img
+              src={session.value.user.image}
+              alt={session.value.user.name}
+              width={96}
+              height={96}
+            />
+          ) : null}
+          <p>{session.value?.user?.email}</p>
+          <button onClick$={() => signOut.submit({ callbackUrl: "/" })}>
+            Sign Out
+          </button>
+        </>
+      ) : (
+        <>
+          <p>Not Signed in</p>
+          <button
+            onClick$={() =>
+              signIn.submit({
+                providerId: "github",
+                options: { callbackUrl: "/" },
+              })
+            }
+          >
+            Sign In
+          </button>
+        </>
+      )}
     </>
   );
 });
